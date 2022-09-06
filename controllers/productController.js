33070ded55e46bc364928f1960735219a75125a7
Cemil Ülay <cemilulay@gmail.com>
2022-09-06 09:54:57 +0200

    add createProduct and updateProduct

--- controllers/productController.js
+++ controllers/productController.js
@@ -25,4 +25,74 @@ const getProductById = asyncHandler(async (req, res) => {
   }
 });
 
-export { getProducts, getProductById };
+// @desc    Delete a product
+// @route   Delete /api/products/:id
+// @access  Private/Admin
+
+const deleteProduct = asyncHandler(async (req, res) => {
+  const product = await Product.findById(req.params.id);
+
+  if (product) {
+    await product.remove();
+    res.json({ message: "Product removed" });
+  } else {
+    res.status(404);
+    throw new Error("Product not found");
+  }
+});
+
+// @desc    Create a product
+// @route   Post /api/products
+// @access  Private/Admin
+
+const createProduct = asyncHandler(async (req, res) => {
+  const product = new Product({
+    name: "Sample modell",
+    price: 0,
+    user: req.user._id,
+    image: "/images/sample.jpg",
+    brand: "Sample märke",
+    category: "Sample kategori",
+    countInStock: 0,
+    numReviews: 0,
+    description: "Sample beskrivning",
+  });
+
+  const createdProduct = await product.save();
+  res.status(201).json(createdProduct);
+});
+
+// @desc    Update a product
+// @route   Put /api/products/:id
+// @access  Private/Admin
+
+const updateProduct = asyncHandler(async (req, res) => {
+  const { name, price, description, image, brand, category, countInStock } =
+    req.body;
+
+  const product = await Product.findById(req.params.id);
+
+  if (product) {
+    product.name = name
+    product.price = price;
+    product.description = description;
+    product.image = image;
+    product.brand = brand;
+    product.category = category;
+    product.countInStock = countInStock;
+
+    const updatedProduct = await product.save();
+    res.json(updatedProduct);
+  } else {
+    res.status(404);
+    throw new Error("Product not found");
+  }
+});
+
+export {
+  getProducts,
+  getProductById,
+  deleteProduct,
+  createProduct,
+  updateProduct,
+};
