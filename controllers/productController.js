7c0927b86be4287968df29d2feb594b868b4ecaa
Cemil Ülay <cemilulay@gmail.com>
2022-09-15 13:28:36 +0200

    add getTopProducts

--- controllers/productController.js
+++ controllers/productController.js
@@ -156,6 +156,17 @@ const createProductReview = asyncHandler(async (req, res) => {
   }
 });
 
+// @desc    Get top rated products
+// @route   Get /api/products/top
+// @access  Public
+
+const getTopProducts = asyncHandler(async (req, res) => {
+    const products = await Product.find({}).sort({ rating: -1}).limit(5)
+
+    res.json(products)
+});
+
+
 export {
   getProducts,
   getProductById,
@@ -163,4 +174,5 @@ export {
   createProduct,
   updateProduct,
   createProductReview,
+  getTopProducts
 };
